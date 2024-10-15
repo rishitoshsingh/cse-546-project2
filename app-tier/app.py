4c4854a60d28c376123fba6e505c1d895722e736
rishitoshsingh <rishitoshs@gmaill.com>
2024-10-14 17:17:57 -0700

    fixed

--- app-tier/app.py
+++ app-tier/app.py
@@ -10,7 +10,7 @@ logger = logging.getLogger(__name__)
 logger.setLevel(logging.INFO)
 formatter = logging.Formatter(fmt="%(asctime)s %(name)s.%(levelname)s: %(message)s", datefmt="%Y.%m.%d %H:%M:%S")
 
-handler = logging.StreamHandler(stream=sys.stdout)
+handler = logging.StreamHandler()
 handler.setFormatter(formatter)
 logger.addHandler(handler)
 
